cddf65ee2795162d67671919ab6cfcd220c34eac
FakaFakaYeah <fakafaka270695@yandex.ru>
2025-08-29 07:47:41 +0300

    update data model

--- app/models/data.py
+++ app/models/data.py
@@ -1,7 +1,10 @@
-from sqlalchemy import DateTime, BigInteger
+from datetime import datetime
+
+from sqlalchemy import BigInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import Base
+from constants import MAX_TEXT_LENGTH
 
 
 class Data(Base):
@@ -9,6 +12,6 @@ class Data(Base):
     __tablename__ = 'data'
 
     id: Mapped[int] = mapped_column(BigInteger(), primary_key=True)
-    text: Mapped[str]
-    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True))
+    text: Mapped[str] = mapped_column(String(length=MAX_TEXT_LENGTH))
+    created_at: Mapped[datetime]
     sequence_number: Mapped[int] = mapped_column(BigInteger())
--- constants.py
+++ constants.py
@@ -1,1 +1,7 @@
+
+# db
 DATABASE_URL = "sqlite+aiosqlite:///must.db"
+
+# app
+MIN_TEXT_LENGTH = 1
+MAX_TEXT_LENGTH = 32
