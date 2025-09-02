4353908e06928346c027b07c4158adf3e1ac4984
FakaFakaYeah <fakafaka270695@yandex.ru>
2025-09-02 13:08:51 +0300

    add create data tests

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite+aiosqlite:///must.db"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
--- tests/config.py
+++ tests/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    TEST_DATABASE_URL: str
+    TEST_DATABASE_URL: str = "sqlite+aiosqlite:///test_must.db"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
